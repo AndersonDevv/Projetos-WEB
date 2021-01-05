8fd5a515ab4b20513a91255b0381a5823a0e70b2
Anderson Silva <andersonsilva.programador@gmail.com>
2021-01-05 10:18:00 -0300

    Initial setup

---
+++ Delivery/back-end/src/main/java/com/foodDelivery/Dlivery/DliveryApplication.java
@@ -0,0 +1,13 @@
+package com.foodDelivery.Dlivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DliveryApplication.class, args);
+	}
+
+}
---
+++ Delivery/back-end/src/test/java/com/foodDelivery/Dlivery/DliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodDelivery.Dlivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
