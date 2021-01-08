d70a71ed7434ee1ac3110259bea0390c430a4678
Anderson Silva <andersonsilva.programador@gmail.com>
2021-01-08 15:10:48 -0300

    Add Home Page

--- Delivery/front-end-web/src/App.tsx
+++ Delivery/front-end-web/src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
+import Home from './home';
 import Navbar from './Navbar';
 function App() {
   return (
     <div>
       <Navbar />
+      <Home />
     </div>
   );
 }
---
+++ Delivery/front-end-web/src/home/index.tsx
@@ -0,0 +1,29 @@
+import './styles.css';
+import { ReactComponent as MainImage} from './entrega.svg';
+function Home(){
+ return (
+    <div className="home-conteiner">
+        <div className="home-content">
+            <div className="home-actions">
+                <h1 className="home-title">
+                    Faça seu pedido <br/> 
+                    que entregamos <br/> 
+                    para você!!!
+                </h1>
+                <h3 className="home-subtitle">
+                    Escolha o seu pedido e em poucos minutos <br/>
+                    levaremos na sua porta
+                </h3>
+                <a href="www" className="home-btn-order">FAZER pedido</a>
+            </div>
+            <div className="home-image">
+               <MainImage />
+            </div>
+        </div>   
+    </div>
+   
+ );
+}
+
+export default Home;
+
